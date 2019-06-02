e0ea78398fee12537571397852cc4a806801c428
natel9178 <natel9178@gmail.com>
2019-06-01 23:52:47 -0700

    optionalized feature limit

--- src/tlgan/feature_axis.py
+++ src/tlgan/feature_axis.py
@@ -30,6 +30,7 @@ def main():
     parser.add_argument('-labels_loc', default='./gan_samples_labels.hdf5')
     parser.add_argument('-samples_loc', default='./gan_samples.hdf5')
     parser.add_argument('-f_save_dir', default='./feature_direction.pkl')
+    parser.add_argument('-features_limit', type=int, default=15)
     opt = parser.parse_args()
 
     with h5py.File(opt.labels_loc, 'r') as l, h5py.File(opt.samples_loc, 'r') as s:
@@ -39,7 +40,7 @@ def main():
         print('Latent vectors took', now - time.time(), 'to load')
         print('Loading labels')
         now = time.time()
-        y = l['labels'][:, :30]
+        y = l['labels'][:, :opt.features_limit]
         print('Labels took', now - time.time(), 'to load')
 
         print('Finding feature axis')
