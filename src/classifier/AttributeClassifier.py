b9a0e35626cf44f0208153e9bc7b96a6805cea12
natel9178 <natel9178@gmail.com>
2019-06-04 14:46:16 -0700

    wip

--- src/classifier/AttributeClassifier.py
+++ src/classifier/AttributeClassifier.py
@@ -9,7 +9,7 @@ import torch.nn.functional as F
 
 import pretrainedmodels as pm
 import pretrainedmodels.utils as utils
-import .mobilenet
+import mobilenet
 from torchsummary import summary
 
 class AttributeClassifierMobileNet(nn.Module):
