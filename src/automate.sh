b987d3bcc6d79e15869cf0c55823830304054adf
natel9178 <natel9178@gmail.com>
2019-06-04 14:50:29 -0700

    full

--- src/automate.sh
+++ src/automate.sh
@@ -1,6 +1,6 @@
-python compute_features.py -batch_size=256 -model_weight_loc=./trained_models/final_model.chkpt -samples_loc=./tlgan/gan_samples
-python compute_features.py -batch_size=256 -model_weight_loc=./trained_models/final_model.chkpt -samples_loc=./tlgan/gan_samples_cats
-python compute_features.py -batch_size=256 -model_weight_loc=./trained_models/final_model.chkpt -samples_loc=./tlgan/gan_samples_figs
-python compute_features.py -batch_size=256 -model_weight_loc=./trained_models/final_model.chkpt -samples_loc=./tlgan/gan_samples_laptop
-python compute_features.py -batch_size=256 -model_weight_loc=./trained_models/final_model.chkpt -samples_loc=./tlgan/gan_samples_pineapple
-python compute_features.py -batch_size=256 -model_weight_loc=./trained_models/final_model.chkpt -samples_loc=./tlgan/gan_samples_washingmachine
+python compute_features.py -batch_size=256 -model_weight_loc=./trained_models/final_model -samples_loc=./tlgan/gan_samples
+python compute_features.py -batch_size=256 -model_weight_loc=./trained_models/final_model -samples_loc=./tlgan/gan_samples_cats
+python compute_features.py -batch_size=256 -model_weight_loc=./trained_models/final_model -samples_loc=./tlgan/gan_samples_figs
+python compute_features.py -batch_size=256 -model_weight_loc=./trained_models/final_model -samples_loc=./tlgan/gan_samples_laptop
+python compute_features.py -batch_size=256 -model_weight_loc=./trained_models/final_model -samples_loc=./tlgan/gan_samples_pineapple
+python compute_features.py -batch_size=256 -model_weight_loc=./trained_models/final_model -samples_loc=./tlgan/gan_samples_washingmachine
